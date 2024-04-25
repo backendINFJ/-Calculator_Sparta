2e62039ed29f8b69cb7542d79ae90ec3cb6bb8a4
yoonjae <backendINFJ@github.com>
2024-04-25 16:21:33 +0900

    주석 수정

--- carculator/Calculator.java
+++ carculator/Calculator.java
@@ -1,6 +1,6 @@
 package carculator;
 
-public class Carculator {
+public class Calculator {
 
     AddOperation addOperation = new AddOperation(); // 덧셈 인스턴스
 
@@ -11,3 +11,5 @@ public class Carculator {
     DivideOperation divideOperation = new DivideOperation(); // 나눗셈 || 나머지 인스턴스
 
 }
+
+// 인스턴스를 생성한 객체 안에 있는
--- carculator/Main.java
+++ carculator/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double result = AddOperation.add(11.1, 10.1); // 덧셈 메서드 호출
+        double result = AddOperation.add(11.1, 10.1); // 덧셈 메서드 호출 // add에 있는걸 호출한거임 인스턴스를 호출한게 아니라
         System.out.println(result);
 
         double result1 = AbstractOperation.min(11.1, 10.1); // 덧셈 메서드 호출
